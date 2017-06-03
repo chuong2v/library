fe5771492dacbe404c5324aa718bbdc4f6c3a543
chuong2v <chuong.2v@gmail.com>
2017-06-03 18:53:14 +0700

    [set up] - api

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
+
 let instance = axios.create({
   baseURL: 'http://localhost:8080/PGELite/sv/',
   timeout: 1000,
-  headers: { 'Access-Control-Allow-Origin': '*' }
+  headers: { 'Content-Type': 'application/json' }
 })
 
 export default instance
