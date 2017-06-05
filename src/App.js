834e9da958ac8696a5d6e065a6cebc25ab6f0254
chuong2v <chuong.2v@gmail.com>
2017-06-05 13:40:53 +0700

    [router]

--- src/App.js
+++ src/App.js
@@ -1,29 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import { fetchGroupsFromApi } from './actions/group'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import {
+  AppBar,
+  MenuItem,
+  Drawer,
+  RaisedButton
+} from 'material-ui'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+  }
   componentDidMount() {
     this.props.fetchGroupsFromApi()
   }
+  handleMenubar() {
+    this.setState({ open: true })
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={this.handleMenubar.bind(this)}
+          >
+          </AppBar>
+          {/*<RaisedButton
+            label="Toggle Drawer"
+            onTouchTap={this.handleMenubar.bind(this)}
+          />*/}
+          <Drawer open={this.state.open}>
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={Login} />
+          <Route path="/topics" component={Login} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      </Router >
+    )
   }
 }
-
 
 function mapStateToProps(state) {
   return {
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MaterialUI from 'material-ui'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        This is login page
+      </div>
+    )
+  }
+}
+
+export default Login
